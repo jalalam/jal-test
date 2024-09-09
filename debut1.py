df4e3632a4a747c9efa9bc5eda6d6b9cafbf0a19
EL MEDAGHRI EL ALAOUI <jalal.el-medaghri-el-alaoui@capgemini.com>
2024-09-09 11:47:03 +0200

    la premiere version

---
+++ debut1.py
@@ -0,0 +1,8 @@
+def call_me():
+    print("hello Jalal")
+
+if __name__ == "__main__":
+    call_me()
+                                                                 
+
+
---
+++ run_me.py
@@ -0,0 +1,1 @@
+from debut1 import call_me
